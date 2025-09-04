519ca00ccf05dd1dbcb53390da0e5637398719fd
thaikiet1108 <thaikiet.0811@gmail.com>
2025-09-04 08:32:19 +0700

    Create bai1.php

---
+++ bai1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $arr = array("rain","sunshine","clouds","hail","sleet","snow","wind") ;
+    echo"We've seen all kinds of weather this month. At the beginning of the month, we had $arr[5] and $arr[6]. 
+    Then came $arr[1] with a few $arr[2] and some $arr[0]. At least we didn't get any $arr[3] or $arr[4].";
+    ?>
+</body>
+</html>
